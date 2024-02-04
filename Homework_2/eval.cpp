7ef5a8e8e51cba899226de4f4d25e8a14c9fc221
kylej21 <you@example.com>
2024-02-03 19:49:01 -0800

    started hw2 problem 5

---
+++ Homework_2/eval.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../Project_2/Map.h"
+using namespace std;
+bool isValid(string initial){
+    if(initial.size()==0){
+        return false;
+    }
+
+}
+int evaluate(string infix, const Map& values, string& postfix, int& result){
+
+}
